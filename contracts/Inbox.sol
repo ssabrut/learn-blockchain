0c6d03ca6ed7c5c3e7428df0cd95063715daff7e
ssabrut <michael.gunawan2002@gmail.com>
2024-01-25 14:09:35 +0700

    First Inbox Contract

---
+++ contracts/Inbox.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
+
+contract Inbox {
+    string public message;
+
+    // The following specifiers are available:
+    // storage: The variable is a state variable (stored on blockchain)
+    // memory: The variable is in memory and it exists while a function is being called
+    // calldata: It is a special data location that contains the function arguments
+
+    // The following function modifiers are available:
+    // public: Accessible by anyone
+    // private: Only accessible by the contract itself
+    // internal: Only accessible by the contract itself and contracts deriving from it
+    // external: Only accessible by other contracts
+
+    // The following common function types are available:
+    // pure: Function does not read from or modify the state
+    // view: Function does not modify the state
+    // payable: Function allows them to receive Ether along with a call
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
