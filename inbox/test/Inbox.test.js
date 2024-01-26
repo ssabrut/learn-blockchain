e499b4f892d7f00b2f26b7a00c90de70ba2260ef
ssabrut <michael.gunawan2002@gmail.com>
2024-01-26 14:04:23 +0700

    Verifying Initial Message

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,11 +2,13 @@ const assert = require('assert');
 const ganache = require('ganache');
 const { describe, it, beforeEach } = require('mocha');
 const { Web3 } = require('web3');
-const web3 = new Web3(ganache.provider()); // ganache provider is a local test network
 const { interface, bytecode } = require('../compile');
 
+const web3 = new Web3(ganache.provider()); // ganache provider is a local test network
 let accounts;
 let inbox;
+const INITIAL_TEST_STRING = 'Hi there!';
+
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
@@ -19,6 +21,11 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox.options.address);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_TEST_STRING);
   });
 });
