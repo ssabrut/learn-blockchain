362d506986dbcc0a61f75dfc359692ebd6380606
ssabrut <michael.gunawan2002@gmail.com>
2024-01-26 23:42:50 +0700

    Testing Message Update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -28,4 +28,10 @@ describe('Inbox', () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, INITIAL_TEST_STRING);
   });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
 });
