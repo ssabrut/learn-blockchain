ecc6dafbf874c80a615331f148b07981ea6404cd
ssabrut <michael.gunawan2002@gmail.com>
2024-01-26 13:21:09 +0700

    Fetching Account from Ganache

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,3 +3,15 @@ const ganache = require('ganache');
 const { describe, it, beforeEach } = require('mocha');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider()); // ganache provider is a local test network
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+
+  // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => { });
+});
